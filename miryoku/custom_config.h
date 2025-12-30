2f4207dbc6dfe88732cafc88157d20dda6b7a96e
LaWankah <jordy@reuzenpanda.nl>
2025-12-30 14:27:20 +0100

    added galium

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -1,3 +1,4 @@
 // Copyright 2021 Manna Harbour
 // https://github.com/manna-harbour/miryoku
-
+#define MIRYOKU_EXTRA_GALLIUM    // This tells the logic file above to use Gallium for "Extra"
+#define MIRYOKU_TAP_GALLIUM      // Optional: This tells it to use Gallium for the "Tap" layer
--- miryoku/miryoku_babel/miryoku_layer_alternatives.h
+++ miryoku/miryoku_babel/miryoku_layer_alternatives.h
@@ -126,6 +126,12 @@ U_MT(LGUI, A),     U_MT(LALT, S),     U_MT(LCTRL, D),    U_MT(LSHFT, F),    &kp 
 U_LT(U_BUTTON, Y), U_MT(RALT, X),     &kp C,             &kp V,             &kp B,             &kp N,             &kp M,             &kp COMMA,         U_MT(RALT, DOT),   U_LT(U_BUTTON, SLASH),\
 U_NP,              U_NP,              U_LT(U_MEDIA, ESC),U_LT(U_NAV, SPACE),U_LT(U_MOUSE, TAB),U_LT(U_SYM, RET),  U_LT(U_NUM, BSPC), U_LT(U_FUN, DEL),  U_NP,              U_NP
 
+#define MIRYOKU_ALTERNATIVES_BASE_GALLIUM \
+&kp B,              &kp L,              &kp D,              &kp C,              &kp V,              &kp J,              &kp Y,              &kp O,              &kp U,              &kp COMMA,          \
+U_MT(LGUI, N),      U_MT(LALT, R),      U_MT(LCTRL, T),     U_MT(LSHFT, S),     &kp G,              &kp P,              U_MT(LSHFT, H),     U_MT(LCTRL, A),     U_MT(LALT, E),      U_MT(LGUI, I),      \
+U_LT(U_BUTTON, X),  U_MT(RALT, Q),      &kp M,              &kp W,              &kp Z,              &kp K,              &kp F,              &kp SQT,            U_MT(RALT, SEMI),   U_LT(U_BUTTON, DOT),\
+U_NP,               U_NP,               U_LT(U_MEDIA, ESC), U_LT(U_NAV, SPACE), U_LT(U_MOUSE, TAB), U_LT(U_SYM, RET),   U_LT(U_NUM, BSPC),  U_LT(U_FUN, DEL),   U_NP,               U_NP
+
 
 #define MIRYOKU_ALTERNATIVES_TAP_AZERTY_FLIP \
 &kp A,             &kp Z,             &kp E,             &kp R,             &kp T,             &kp Y,             &kp U,             &kp I,             &kp O,             &kp P,             \
@@ -247,6 +253,11 @@ U_NP,              U_NP,              &kp ESC,           &kp SPACE,         &kp 
 &kp Y,             &kp X,             &kp C,             &kp V,             &kp B,             &kp N,             &kp M,             &kp COMMA,         &kp DOT,           &kp SLASH,         \
 U_NP,              U_NP,              &kp ESC,           &kp SPACE,         &kp TAB,           &kp RET,           &kp BSPC,          &kp DEL,           U_NP,              U_NP
 
+#define MIRYOKU_ALTERNATIVES_TAP_GALLIUM \
+&kp B,              &kp L,              &kp D,              &kp C,              &kp V,              &kp J,              &kp Y,              &kp O,              &kp U,              &kp COMMA,          \
+&kp N,              &kp R,              &kp T,              &kp S,              &kp G,              &kp P,              &kp H,              &kp A,              &kp E,              &kp I,              \
+&kp X,              &kp Q,              &kp M,              &kp W,              &kp Z,              &kp K,              &kp F,              &kp SQT,            &kp SEMI,           &kp DOT,            \
+U_NP,               U_NP,               &kp ESC,            &kp SPACE,          &kp TAB,            &kp RET,            &kp BSPC,           &kp DEL,            U_NP,               U_NP
 
 #define MIRYOKU_ALTERNATIVES_NAV_INVERTEDT_FLIP \
 &kp PG_UP,         &kp HOME,          &kp UP,            &kp END,           &kp INS,           U_NA,              &u_to_U_BASE,      &u_to_U_EXTRA,     &u_to_U_TAP,       U_BOOT,            \
